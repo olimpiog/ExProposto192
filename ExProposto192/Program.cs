897749d11220293b1b472bcf0316233bd519cbb2
olimpiog <olimpio.gonzatto@gmail.com>
2020-12-16 23:20:51 -0300

    Comite inicial.

---
+++ ExProposto192/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+
+namespace ExProposto192
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o path absolut do arquivo: ");
+            string PathAbsolutoOrigem = Console.ReadLine();
+
+            string PathAbsolutoDestino = Path.GetDirectoryName(PathAbsolutoOrigem) + @"\out\"+Path.GetFileName(PathAbsolutoOrigem);
+
+            string[] Arq = File.ReadAllLines(PathAbsolutoOrigem);
+
+            Directory.CreateDirectory(Path.GetDirectoryName(PathAbsolutoDestino));
+
+            using (StreamWriter sw = File.AppendText(PathAbsolutoDestino))
+            {
+                foreach (string item in Arq)
+                {
+                    string[] linha = item.Split(',');
+
+                    double total = double.Parse(linha[1]) * double.Parse(linha[2]);
+                    sw.WriteLine(linha[0]+", "+total.ToString("F2",CultureInfo.InvariantCulture));
+                }                
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ExProposto192
+# Exercício Proposto192
+# Curso C# COMPLETO 2020 Programação Orientada a Objetos + Projetos
+# Udemy - Professor Nelio Alves
